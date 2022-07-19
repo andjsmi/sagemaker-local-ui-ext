fc31b3058c34949b6cc548ee3ff9c9b523d18737
andjsmi <andjsmi@amazon.com>
2022-07-19 11:46:30 +1000

    Take a list of instance ids and types

--- src/index.tsx
+++ src/index.tsx
@@ -11,7 +11,7 @@ import {Panel} from '@phosphor/widgets'
 import Tabs from './Tabs'
 import Tab from './Tab'
 
-import { exec } from "child_process";
+//import {exec} from 'child_process'
 
 
 /**
@@ -49,7 +49,7 @@ function MainComponent() {
     <body id='main'>
       <div className='jp-Mainwidget'><h2>SDocker</h2></div>
       <Tabs>
-        <Tab title="Hosts"> <HostsComponent /> </Tab>
+        <Tab title="Hosts"> <CreateHost /> <table><tbody><HostsComponent /></tbody></table> </Tab>
         <Tab title='Images'>Nothing here yet</Tab>
         <Tab title="Containers">No content yet</Tab>
       </Tabs>
@@ -64,28 +64,65 @@ function HostsComponent() {
    * 
    */
 
+  const instances = [
+    ['p3.2xlarge', 'i-23123123', 'active'],
+    ['m5.4xlarge', 'i-abcd', 'not-active']
+  ]
+
 
   return(
-    <table>
-      <tbody>
+    <> {
+      instances.map( (item: Array<String>, i) =>
+        <DockerHostRow instancetype={item[0]} instanceid={item[1]} contextstatus={item[2]} />
+      )
+    }</>
+    // <table>
+    //   <tbody>
 
 
-        <DockerHostRow instancetype='p3.2xlarge' />
+        // instances.map((item, i) => <DockerHostRow instancetype={item[0]} instanceid={item[1]})
+        // <DockerHostRow instancetype='p3.2xlarge' instanceid="i-0123123123213"/>
 
-      </tbody>
-    </table>
+      /* </tbody>
+    </table> */
 
   )
 }
 
+function Dropdown(props: any){
+  return(
+    <label>
+      {props.label}
+      <select value={props.value} onChange={props.onChange}>
+        {props.options.map( (option: { value: string | number | string[]; }) => (
+          <option value={option.value}>option.label</option>
+        ))}
+      </select>
+    </label>
+  )
+}
+
 function DockerHostRow(props: any) {
+  const pauseButtonClickHandler = (event: React.MouseEvent<HTMLButtonElement>, instanceID: String) => {
+    //console.log(event)
+    console.log("Stopping EC2 instance with ID ", instanceID)
+  }
+
+  const stopButtonClickHandler = (event: React.MouseEvent<HTMLButtonElement>, instanceID: String) => {
+    //console.log(event)
+    console.log("Terminating EC2 instance with ID ", instanceID)
+  }
+  
   return(
     <tr>
       <span className='info'>
         <span>Instance Type: {props.instancetype}</span>
       </span>
       <span>
-        <button>Off</button>
+        <span className={props.contextstatus}></span>
+        <span className="State">Running</span>
+        <button onClick={event=> pauseButtonClickHandler(event, props.instanceid)}>II</button>
+        <button onClick={event => stopButtonClickHandler(event, props.instanceid)}>X</button>
       </span>
     </tr>
 
